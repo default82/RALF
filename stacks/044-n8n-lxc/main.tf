84688696ab626e922ec482fef2fbccb7145cf9df
default82 <kolja.otta@gmail.com>
2026-02-23 20:36:39 +0100

    Add n8n LXC and config stacks with PostgreSQL

---
+++ stacks/044-n8n-lxc/main.tf
@@ -0,0 +1,49 @@
+resource "proxmox_virtual_environment_container" "n8n" {
+  node_name     = var.node_name
+  vm_id         = 4030
+  unprivileged  = true
+  started       = true
+  start_on_boot = true
+  features {
+    nesting = true
+  }
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 2048
+    swap      = 0
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 16
+  }
+
+  network_interface {
+    name   = "eth0"
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    hostname = "n8n"
+
+    ip_config {
+      ipv4 {
+        address = "10.10.40.30/16"
+        gateway = "10.10.0.1"
+      }
+    }
+
+    user_account {
+      keys = [var.ssh_public_key]
+    }
+  }
+
+  operating_system {
+    template_file_id = var.lxc_template_id
+    type             = "ubuntu"
+  }
+}

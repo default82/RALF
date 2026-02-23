546c1d4c3f083cf72f40ce2f6b5b423e18d2aead
default82 <kolja.otta@gmail.com>
2026-02-23 13:46:09 +0100

    Update variables.tf

--- stacks/030-minio-lxc/variables.tf
+++ stacks/030-minio-lxc/variables.tf
@@ -1,8 +1,22 @@
-variable "pm_api_url" { type = string }
-variable "pm_api_token_id" { type = string sensitive = true }
-variable "pm_api_token_secret" { type = string sensitive = true }
-variable "node_name" { type = string default = "pve-deploy" }
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "node_name" {
+  type = string
+}
 
 variable "ssh_public_key" {
-  type = string
+  type      = string
+  sensitive = true
 }
